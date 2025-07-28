522452980a7dcc7820e30fa4dd00f79437f0ea11
João Mendes <mendes19966@gmail.com>
2025-07-28 08:58:58 +0100

    a

--- Aulas Python/Antonio/misc/example2.sql
+++ Aulas Python/Antonio/misc/example2.sql
@@ -17,15 +17,3 @@ PRIMARY KEY(user2,Post),
 CONSTRAINT fk_post_liked_user2 FOREIGN KEY(user2) REFERENCES user2(num),
 CONSTRAINT fk_post_liked_post FOREIGN KEY(POST) REFERENCES POST(num)
 );
-
-/* 
-1- Crirar DB countries 
-2- Crirar TABLE continents com as seguintes colunas:
-    - continent_id inteiro auto increment PK
-    - name string de 255 caracteres not null
-1- Crirar TABLE languages com as colunas:
-    - region_id inteiro auto increment PK
-    - continent_id inteiro
-    - name string de 100 caracteres not null
-    - fk enrtre as colunas continent_id desta tabela
-*/
